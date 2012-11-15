1afd1c434e51fbf71b07bb1548809f0b491a4c07
vanessa <vanieu2009@hotmail.com>
2012-11-14 23:31:02 -0200

    segundo

---
+++ exercicios/exercicio_1.php
@@ -0,0 +1,14 @@
+<?php
+    $title="Site.com";
+	$subtitle="este é um site em PHP";
+?>
+
+<!doctype html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title><?php echo $title. "-". $subtitle; ?></title>
+</head>
+<body>
+	</body>
+	</html>
